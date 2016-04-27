1d2398fdd656ee259e63b33a15dd939c2ab0c76a
cthorey <clement.thorey@gmail.com>
2016-04-27 10:01:53 +0200

    Update config

--- init.el
+++ init.el
@@ -50,6 +50,9 @@
 (add-hook 'python-mode-hook (lambda ()
                               (require 'sphinx-doc)
                               (sphinx-doc-mode t)))
+;; sql setting
+(require 'sql-setting)
+
 ;; ;-----------;
 ;; ;;; Modes ;;;
 ;; ;-----------;
@@ -73,6 +76,24 @@
 ;; LaTeX and Auctex
 (require 'latex-settings)
 (add-hook 'LaTeX-mode-hook #'latex-extra-mode)
+
+;; web-mode
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(setq web-mode-enable-auto-pairing t)
+(setq web-mode-enable-css-colorization t)
+(setq web-mode-enable-block-face t)
+(setq web-mode-ac-sources-alist
+  '(("css" . (ac-source-css-property))
+    ("html" . (ac-source-words-in-buffer ac-source-abbrev))))
+
 
 ;; Todo.txt
 (add-to-list 'load-path "~/Dropbox/todo/")
