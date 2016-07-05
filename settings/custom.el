a38885389a368bc12360880b10bc1bbfe8f1e7d1
cthorey <clement.thorey@gmail.com>
2016-07-05 16:53:12 +0200

    ok

--- settings/custom.el
+++ settings/custom.el
@@ -8,6 +8,11 @@
  '(custom-safe-themes
    (quote
     ("705f3f6154b4e8fac069849507fd8b660ece013b64a0a31846624ca18d6cf5e1" "a1289424bbc0e9f9877aa2c9a03c7dfd2835ea51d8781a0bf9e2415101f70a7e" "a31370120e58f7a7a5f111c1d3167e895c8e48a42b8c0a6026e1edb9b29a5fb5" "4904daa168519536b08ca4655d798ca0fb50d3545e6244cefcf7d0c7b338af7e" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
- '(markdown-command
-   "/Users/thorey/miniconda2/bin/markdown2"))
+ '(markdown-command "/Users/thorey/miniconda2/bin/markdown2"))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- settings/sql-setting.el
+++ settings/sql-setting.el
@@ -2,4 +2,33 @@
           (lambda ()
             (toggle-truncate-lines t)))
 
+(setq sql-connection-alist
+      '((server1 (sql-product 'postgres)
+                  (sql-port 5432)
+                  (sql-server "localhost")
+                  (sql-user "user")
+                  (sql-password "password")
+                  (sql-database "db1"))
+        (server2 (sql-product 'postgres)
+                  (sql-port 32770)
+                  (sql-server "192.168.99.101")
+                  (sql-user "docker")
+                  (sql-password "docker")
+                  (sql-database "db2"))))
+
+(defun my-sql-server1 ()
+  (interactive)
+  (my-sql-connect 'postgres 'server1))
+
+(defun my-sql-server2 ()
+  (interactive)
+  (my-sql-connect 'postgres 'server2))
+
+(defun my-sql-connect (product connection)
+  ;; remember to set the sql-product, otherwise, it will fail for the first time
+  ;; you call the function
+  (setq sql-product product)
+  (sql-connect connection))
+
+
 (provide 'sql-setting)
